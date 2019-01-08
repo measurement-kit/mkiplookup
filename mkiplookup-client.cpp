ca0f067bb19b9f11524954ba638fa87931c45ada
Simone Basso <bassosimone@gmail.com>
2019-01-08 10:32:53 +0100

    Rewrite to have C++ API

--- mkiplookup-client.cpp
+++ mkiplookup-client.cpp
@@ -3,28 +3,25 @@
 #include <iostream>
 #include <ios>
 
-#include "mkiplookup.h"
+#include "mkiplookup.hpp"
 
 #define MKDATA_INLINE_IMPL
-#include "mkdata.h"
+#include "mkdata.hpp"
 #define MKCURL_INLINE_IMPL
-#include "mkcurl.h"
+#include "mkcurl.hpp"
 
 int main() {
-  mkiplookup_request_uptr request{mkiplookup_request_new_nonnull()};
-  mkiplookup_request_set_ca_bundle_path(request.get(), "ca-bundle.pem");
-  mkiplookup_request_set_timeout(request.get(), 7);
-  mkiplookup_response_uptr response{
-    mkiplookup_request_perform_nonnull(request.get())};
-  std::clog << "good: " << std::boolalpha << (bool)mkiplookup_response_good(
-      response.get()) << std::endl;
-  std::clog << "bytes_sent: " << mkiplookup_response_get_bytes_sent(
-      response.get()) << std::endl;
-  std::clog << "bytes_recv: " << mkiplookup_response_get_bytes_recv(
-      response.get()) << std::endl;
-  std::clog << "probe_ip: " << mkiplookup_response_get_probe_ip(
-      response.get()) << std::endl;
+  mk::iplookup::Request request;
+  request.ca_bundle_path = "ca-bundle.pem";
+  request.timeout = 7;
+  mk::iplookup::Response response = mk::iplookup::perform(request);
+  std::clog << "good: " << std::boolalpha << response.good << std::endl;
+  std::clog << "bytes_sent: " << response.bytes_sent << std::endl;
+  std::clog << "bytes_recv: " << response.bytes_recv << std::endl;
+  std::clog << "probe_ip: " << response.probe_ip << std::endl;
   std::clog << "=== BEGIN LOGS ===" << std::endl;
-  std::clog << mkiplookup_response_moveout_logs(response);
+  for (auto &line : response.logs) {
+    std::clog << line << std::endl;
+  }
   std::clog << "=== END LOGS ===" << std::endl;
 }
